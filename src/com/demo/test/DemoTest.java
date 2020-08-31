c233d1dcb210c24426f2a909995fec3ccb1de038
moo <moo@163.com>
2020-08-31 17:24:17 +0800

    Fix bugs.

--- src/com/demo/test/DemoTest.java
+++ src/com/demo/test/DemoTest.java
@@ -25,7 +25,7 @@ public class DemoTest {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfiguration.class);
 //		Object bean = context.getBean("demo");
 //		System.out.println(bean);
-		Thread.sleep(3000000);
+		Thread.sleep(5000000);
 		context.close();
 	}
 }
